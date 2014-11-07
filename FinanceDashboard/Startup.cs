57756943eb92047fd57fcdb398637a905d550b79
pedromaximiano <pedro.maximiano@gmail.com>
2014-11-07 18:41:24 +0000

    Initial commit.

---
+++ FinanceDashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanceDashboard.Startup))]
+namespace FinanceDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
